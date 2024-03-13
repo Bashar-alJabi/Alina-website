a85b9bdc98f6749ba0816ecb7900a5f956d7ecf2
Bashar-alJabi <basharaljabi.dev@gmail.com>
2024-03-13 14:42:34 +0300

    Tiktok link updated

--- src/components/Navbar/data.ts
+++ src/components/Navbar/data.ts
@@ -1,7 +1,7 @@
 export const social_media = [
     {
         id: 1,
-        link: 'https://www.tiktok.com/@bbli83?_t=8kQTNnIymbB&_r=1',
+        link: 'https://www.tiktok.com/@bbli_official?_t=8kdtJqPDW77&_r=1',
         path: "M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z",
     },
     {
